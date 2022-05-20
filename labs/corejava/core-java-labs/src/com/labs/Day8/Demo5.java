e41ebdc4e51985d19d63e08830d97478efba854b
Swarna <swarna.mallela@gmail.com>
2022-05-20 14:28:39 +0530

    example on finally

---
+++ labs/corejava/core-java-labs/src/com/labs/Day8/Demo5.java
@@ -0,0 +1,37 @@
+package com.labs.Day8;
+
+
+
+public class Demo5 {
+    public static void main(String[] args) {
+        // case 1: exception not occur
+        try {
+            int c = 10 / 5;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("Case 1: finally called");
+        }
+
+        // case 2: exception occur but not handled
+        try {
+            int c = 20 / 0;
+            System.out.println(c);
+        }catch (Exception ex){
+
+        }
+        finally {
+            System.out.println("Case 2: finally called");
+        }
+
+        // cse 3: exception occur but handled
+        try {
+            int c = 20 / 0;
+            System.out.println(c);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("Case 3: finally called");
+        }
+    }
+}
