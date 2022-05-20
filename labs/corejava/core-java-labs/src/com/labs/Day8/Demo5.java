9e4605fe4cdde58c97dc40d691bb93cbf762b934
Swarna <swarna.mallela@gmail.com>
2022-05-20 16:46:18 +0530

    modified

--- labs/corejava/core-java-labs/src/com/labs/Day8/Demo5.java
+++ labs/corejava/core-java-labs/src/com/labs/Day8/Demo5.java
@@ -4,7 +4,7 @@ package com.labs.Day8;
 
 public class Demo5 {
     public static void main(String[] args) {
-        // case 1: exception not occur
+        // case 1: exception not occurred
         try {
             int c = 10 / 5;
         } catch (Exception ex) {
@@ -13,7 +13,7 @@ public class Demo5 {
             System.out.println("Case 1: finally called");
         }
 
-        // case 2: exception occur but not handled
+        // case 2: exception occurred but not handled
         try {
             int c = 20 / 0;
             System.out.println(c);
@@ -24,7 +24,7 @@ public class Demo5 {
             System.out.println("Case 2: finally called");
         }
 
-        // cse 3: exception occur but handled
+        // case 3: exception occurred but handled
         try {
             int c = 20 / 0;
             System.out.println(c);
